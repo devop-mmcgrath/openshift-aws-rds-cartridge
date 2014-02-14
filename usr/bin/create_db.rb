8cc092500a886231056f5f58e2e1b00f7fe435f4
Mike McGrath <mmcgrath@redhat.com>
2014-02-14 14:30:37 -0600

    adding create db section

---
+++ usr/bin/create_db.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk'
+require 'nokogiri'
+
+
+# Using environment variables we don't have to provide them in this call
+rds = AWS::RDS.new()
+
+db_name=ENV['OPENSHIFT_APP_NAME']
+
+# Add check to not create db instance twice
+
+db = rds.client.create_db_instance(
+                            :db_name => db_name,
+                            :db_instance_identifier => db_name,
+                            :allocated_storage => 5,
+                            :db_instance_class => 'db.m1.medium',
+                            :engine => 'mysql',
+                            :master_username => 'admin',
+                            :master_user_password => '12345678')
+
+
+while rds.client.describe_db_instances(:db_instance_identifier => db_name).db_instances[0].db_instance_status == 'creating' do
+    puts "creating db"
+    sleep(5)
+end
+
+created_db = rds.client.describe_db_instances(:db_instance_identifier => db_name)
+
+puts db_created.db_instances[0].endpoint.address
+puts db_created.db_instances[0].endpoint.port
---
+++ usr/bin/teardown_db.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk'
+require 'nokogiri'
+
+
+# Using environment variables we don't have to provide them in this call
+rds = AWS::RDS.new()
+
+db_name=ENV['OPENSHIFT_APP_NAME']
+
+
+# Create a snapshot of database on remove
+db = rds.client.delete_db_instance(
+    :db_instance_identifier => db_name,
+    :final_db_snapshot_identifier => db_name + '-' + Time.now.to_i.to_s)
+
+puts "DB Instance Status: " + db.db_instance_status
+
